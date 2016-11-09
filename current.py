559a48ff795a5ddb32ac7ce3611bb6ac82eb9dd1
JotoEnergy <raspijoto@gmail.com>
2016-11-09 22:16:17 +0000

    Port Optimierung
    Port Optimierungen und Definitionen

--- current.py
+++ current.py
@@ -2,9 +2,9 @@ import spidev
 import time
 import os
 
-pin_number = int(input('Type in the chip pin number you want to use: '))
+#pin_number = int(input('Type in the chip pin number you want to use: '))
 # Define delay between readings
-delay = int(input('Type delay between readings: '))
+delay = 1 #int(input('Type delay between readings: '))
 
 # Open SPI bus
 spi = spidev.SpiDev()
@@ -19,6 +19,20 @@ def ReadChannel(channel):
     data = ((adc[1] & 3) << 8) + adc[2]
     return data
 
+def ReadChannelBat(channel):
+    if channel > 7 or channel < 0:
+        return -1
+    adc = spi.xfer2([1, (8 + channel) << 4, 0])
+    Bat = ((adc[1] & 3) << 8) + adc[2]
+    return Bat
+
+def ReadChannelSolar(channel):
+    if channel > 7 or channel < 0:
+        return -1
+    adc = spi.xfer2([1, (8 + channel) << 4, 0])
+    Solar = ((adc[1] & 3) << 8) + adc[2]
+    return Solar
+
 # Function to convert data to voltage level,
 # rounded to specified number of decimal places.
 def ConvertVolts(data, places):
@@ -27,11 +41,28 @@ def ConvertVolts(data, places):
     volts = round(volts, places)
     return volts
 
+def ConvertVoltsBat(Bat, places):
+    #return .0264 * data - 13.51
+    voltsBat = (Bat * 3.3) / float(1023)
+    voltsBat = round(voltsBat, places)
+    return voltsBat
+
+def ConvertVoltsSolar(Solar, places):
+    #return .0264 * data - 13.51
+    voltsSolar = (Solar * 3.3) / float(1023)
+    voltsSolar = round(voltsSolar, places)
+    return voltsSolar
+
 while True:
     # Read the light sensor data
-    current_level = ReadChannel(pin_number)
-    current_volts = ConvertVolts(current_level, 2)
+    Verbraucher_level = ReadChannel(0)
+    Verbraucher_volts = ConvertVolts(Verbraucher_level, 3)
+    
+    Batterie_level = ReadChannelBat(1)
+    Batterie_volts = ConvertVoltsBat(Batterie_level, 3)
 
+    Solarpanel_level = ReadChannelSolar(2)
+    Solarpanel_volts = ConvertVoltsSolar(Solarpanel_level, 3)
     # Read the temperature sensor data
     #temp_level = ReadChannel(temp_channel)
     #temp_volts = ConvertVolts(temp_level, 2)
@@ -39,7 +70,9 @@ while True:
 
     # Print out results
     print ("--------------------------------------------")
-    print("Current Laptop: {} ({}V)".format(current_level, current_volts))
+    print("Verbraucher: {} ({}V)".format(Verbraucher_level, Verbraucher_volts))
+    print("Batterie   : {} ({}V)".format(Batterie_level, Batterie_volts))
+    print("Solarpanel : {} ({}V)".format(Solarpanel_level, Solarpanel_volts))
     #print("Temp : {} ({}V) {} deg C".format(temp_level, temp_volts, temp))
 
     # Wait before repeating loop
