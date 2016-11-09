6450a063385c9130681988e3d5e0dacff99ddc32
Tobias Schwarz <toschdev@gmail.com>
2016-11-10 00:27:29 +0100

    test input1

--- current.py
+++ current.py
@@ -10,43 +10,32 @@ delay = 1 #int(input('Type delay between readings: '))
 spi = spidev.SpiDev()
 spi.open(0, 0)
 
+
+
+
 # Function to read SPI data from MCP3008 chip
 # Channel must be an integer 0-7
-def ReadChannel(channel):
-    if channel > 7 or channel < 0:
-        return -1
-    adc = spi.xfer2([1, (8 + channel) << 4, 0])
-    data = ((adc[1] & 3) << 8) + adc[2]
-    return data
-
-def ReadChannelBat(channel):
-    if channel > 7 or channel < 0:
-        return -1
-    adc = spi.xfer2([1, (8 + channel) << 4, 0])
-    Bat = ((adc[1] & 3) << 8) + adc[2]
-    return Bat
-
-def ReadChannelSolar(channel):
-    if channel > 7 or channel < 0:
-        return -1
-    adc = spi.xfer2([1, (8 + channel) << 4, 0])
-    Solar = ((adc[1] & 3) << 8) + adc[2]
-    return Solar
-
 def readChannel(channel):
-    #Channel must be between 1, 6
     adc = spi.xfer2([1, (8 + channel) << 4, 0])
     data = ((adc[1] & 3) << 8) + adc[2]
     return data
 
 # Function to convert data to voltage level,
 # rounded to specified number of decimal places.
+def convertVolt (input1):
 
-def convertVolt (input1, decimals):
-
-    return round((input1 * 3.3) / float(1024), decimals)
+    return (input1 * 3.3) / float(1024)
 
 #def newConvertVolt (input1, decimals):
+
+
+def current (channel):
+    return readChannel(channel)
+
+
+
+
+
 
 while True:
 
@@ -55,20 +44,23 @@ while True:
     solarStorage = []
 
     for i in xrange(1, 10):
-        # Read the light sensor data
+
         #Convert to Volts
-        verbraucherVolts = convertVolt(readChannel(0), 3)
-        batteryVolts = convertVolt(readChannel(1), 3)
-        solarVolts = convertVolt(readChannel(2), 3)
+        #verbraucherVolts = convertVolt(readChannel(0))
+        #batteryVolts = convertVolt(readChannel(1))
+        #solarVolts = convertVolt(readChannel(2))
+
+        #Read Bitdata and linear function
+
 
         #Write to array and create average
-        verbraucherStorage.append(verbraucherVolts)
-        batteryStorage.append(batteryVolts)
-        solarStorage.append(solarVolts)
+        #verbraucherStorage.append(verbraucherVolts)
+        #batteryStorage.append(batteryVolts)
+        solarStorage.append(current(2))
         time.sleep(0.1)
 
-    averageVerbraucher = round(reduce(lambda x, y: x + y, verbraucherStorage) / len(verbraucherStorage),3)
-    averageBattery = round(reduce(lambda x, y: x + y, batteryStorage) / len(batteryStorage),3)
+    #averageVerbraucher = round(reduce(lambda x, y: x + y, verbraucherStorage) / len(verbraucherStorage),3)
+   # averageBattery = round(reduce(lambda x, y: x + y, batteryStorage) / len(batteryStorage),3)
     averageSolar = round(reduce(lambda x, y: x + y, solarStorage) / len(solarStorage),3)
 
     # Print out results
@@ -77,8 +69,8 @@ while True:
 #Batterie_level
 #Solarpanel_level
     print ("--------------------------------------------")
-    print("Verbraucher: ({}V)".format(averageVerbraucher))
-    print("Batterie   : ({}V)".format(averageBattery))
+    #print("Verbraucher: ({}V)".format(averageVerbraucher))
+    #print("Batterie   : ({}V)".format(averageBattery))
     print("Solarpanel : ({}V)".format(averageSolar))
     #print("Temp : {} ({}V) {} deg C".format(temp_level, temp_volts, temp))
 
