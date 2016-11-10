99d82721f8d88156c8a0bdf80eb96c86e1dee2d4
Tobias Schwarz <toschdev@gmail.com>
2016-11-10 12:32:52 +0100

    GUI

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 var PythonShell = require('python-shell');
+var express = require('express');
 
+
+var app = express();
+app.use(express.static('public'));
+
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.listen(3000);
 
 setInterval(function() {
     PythonShell.run('current.py', function (err, data) {
