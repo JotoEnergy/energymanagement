25be30e5d2978f1effe30f0dd7df3ccf0b171d65
Tobias Schwarz <toschdev@gmail.com>
2016-11-10 10:58:25 +0100

    db

--- database.py
+++ database.py
@@ -16,7 +16,7 @@ device = "Verbraucher"
 power = 10
 volt = 1.5
 # Use all the SQL you like
-cursor.execute("INSERT INTO powerSensor (datum, power, volt, device) VALUES ('{}', '{}', '{}', '{}')".format(now, power, volt, device) )
+cursor.execute("INSERT INTO powerSensor (datum, power, volt, device) VALUES ('{}', '{}', '{}', '{}');".format(now, power, volt, device) )
 
 try:
     # Execute the SQL command
