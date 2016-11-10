b81c92385b5e25d60e36c1213ce573ce958c8391
Tobias Schwarz <toschdev@gmail.com>
2016-11-10 12:05:21 +0100

    interval

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 var PythonShell = require('python-shell');
 
 
-
-PythonShell.run('current.py', function (err, data) {
-    if (err) throw err;
-    console.log(data);
-});
+setInterval(function() {
+    PythonShell.run('current.py', function (err, data) {
+        if (err) throw err;
+        console.log(data);
+    })
+}, 5000);
