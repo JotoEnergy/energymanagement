f402a0931e37bbeadc07e32d60acabb3d454fdd0
Tobias Schwarz <toschdev@gmail.com>
2016-11-10 11:06:26 +0100

    emptyDB

---
+++ emptyDatabase.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+import MySQLdb
+import time
+
+db = MySQLdb.connect(host="localhost",    # your host, usually localhost
+                     user="root",         # your username
+                     passwd="joto123",  # your password
+                     db="raspi")        # name of the data base
+
+# you must create a Cursor object. It will let
+#  you execute all the queries you need
+cursor = db.cursor()
+
+now = time.time()
+device = "Verbraucher"
+power = 10
+volt = 1.5
+# Use all the SQL you like
+
+
+try:
+    # Execute the SQL command
+    cursor.execute("DELET * FROM powerSensor" )
+    # Commit your changes in the database
+    db.commit()
+except:
+    # Rollback in case there is any error
+    db.rollback()
+
+db.close()
