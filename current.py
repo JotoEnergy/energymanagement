0afaffef2429b37ba75818f4c80c1e1b737bb70e
Tobias Schwarz <toschdev@gmail.com>
2016-11-16 21:29:34 +0100

    test

--- current.py
+++ current.py
@@ -29,7 +29,7 @@ offset = [300, 0, 550]
 # Function to read SPI data from MCP3008 chip
 # Channel must be an integer 0-7
 def readChannel(channel):
-    adc = spi.xfer2([1, (8 + channel) << 4, 0])
+    adc = spi.xfer2([1, (8 + channel), 0])
     data = ((adc[1] & 3) << 8) + adc[2]
     return data
 
