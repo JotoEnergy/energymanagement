1bd1ca395bbbba900cfd6420441e91afa6484b30
Tobias Schwarz <toschdev@gmail.com>
2016-11-16 21:16:32 +0100

    bug

--- test.py
+++ test.py
@@ -11,7 +11,7 @@ spi.open(0, 0)
 def readChannel(channel):
     adc = spi.xfer2([1, (8 + channel) << 4, 0])
 
-    print("adc: ".format(adc)
+    print("adc: ".format(adc))
 
     data = ((adc[1] & 3) << 8) + adc[2]
     return data
