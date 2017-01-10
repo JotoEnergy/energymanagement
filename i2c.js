efbe279169a7a0ff3040726b717d037ee590ba12
Tobias Schwarz <toschdev@gmail.com>
2017-01-10 22:33:40 +0100

    update console output i2c

--- i2c.js
+++ i2c.js
@@ -3,23 +3,22 @@ var ina219 = require('ina219');
 module.exports = {
     readi2c: function () {
 
-        var ampereAndVolt = {};
         ina219.init();
-        ina219.enableLogging(true);
+        //ina219.enableLogging(true);
 
         ina219.calibrate32V1A(function () {
 
             ina219.getBusVoltage_V(function (volts) {
 
-                //console.log("Voltage: " + volts);
+                console.log("Voltage: " + volts);
 
                 ina219.getCurrent_mA(function (current){
+                    console.log("Current (mA): " + current );
 
-                    ampereAndVolt = {volts: volts, current: current};
-
-                    //console.log("Current (mA): " + current );
-
-                    return ampereAndVolt;
+                    return {
+                        volts: volts,
+                        current: current
+                    };
 
                 });
 
