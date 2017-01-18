6e946e20314f74c625d733c8c2deb0a968833b42
Tobias Schwarz <toschdev@gmail.com>
2017-01-18 23:11:10 +0100

    i2c address

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ function readi2cAndWriteIntoDatabase(address, id) {
     }
     */
 
-    var useAddress = address.toString();
+    var useAddress = '1000000';
     var ampereAndVolt1 = i2c.readi2c(useAddress, function(voltAndAmpere) {
 
         var logAmpereAndVolt = JSON.stringify(voltAndAmpere);
