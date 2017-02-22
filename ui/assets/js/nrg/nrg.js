7c498357dd8c61ab0a450463877fc46240df19a4
Tobias Schwarz <toschdev@gmail.com>
2017-02-22 20:04:19 +0100

    GUI update

---
+++ ui/assets/js/nrg/nrg.js
@@ -0,0 +1,140 @@
+var NRG = (function (NRG, $, undefined) {
+    "use strict";
+
+    NRG.init = function() {
+
+        NRG.socket = io.connect();
+        NRG.socket.on('check', function (data) {
+            //console.log(data);
+            console.log('Socket connected');
+            NRG.socket.emit('established');
+        });
+
+        NRG.socket.on('updates', function (data) {
+            console.log(data);
+
+            var dataArray = data.data;
+            var amountData = dataArray.length;
+
+            NRG.inputData = dataArray;
+            NRG.powerData = {
+                "datum": [],
+                "power": []
+            } ;
+
+            //Iterate through every maxVal item
+            var maxVal = 50;
+            var delta = Math.floor( NRG.inputData.length / maxVal );
+            for (var i = 0; i < NRG.inputData.length; i=i+delta) {
+                console.log('Enter: ', NRG.inputData[i].datum, NRG.inputData[i].power);
+
+                var x = new Date();
+                x.setTime(NRG.inputData[i].datum * 1000);
+
+                NRG.powerData.datum.push(x.toLocaleString());
+                NRG.powerData.power.push(NRG.inputData[i].power);
+            }
+
+
+            var lastItem = dataArray[amountData-1];
+            var power = lastItem.power;
+            var inputPower = 0;
+            var outputPower = 0;
+            if(lastItem.power < 0) {
+                inputPower = lastItem.power * -1;
+            } else {
+                outputPower = lastItem.power;
+            }
+            //console.log(value.power)
+            var difference = outputPower - inputPower;
+            var powerPrice = Math.round((difference / 1000) * 0.25 * 1000) / 1000;
+            $("#price_kwh").html(powerPrice +  ' € / h');
+            $("#total_incoming_power").html(inputPower + 'W');
+            $("#total_devices").html('1');
+            $("#power_output").html(outputPower + 'W');
+
+        });
+
+    };
+
+    NRG.formatOutputNumber = function (number) {
+
+        number = Math.floor(number * 1000) / 1000;
+
+        return Decimal(number);
+
+    };
+
+
+    return NRG;
+}(NRG || {}, jQuery));
+
+
+$(document).ready(function() {
+
+    //NRG.init();
+
+    NRG.socket = io.connect();
+    NRG.socket.on('check', function (data) {
+        //console.log(data);
+        console.log('Socket connected');
+        NRG.socket.emit('established');
+    });
+
+    NRG.socket.on('data', function(data) {
+
+        var volt = data.data.volts;
+        var current = data.data.current / 1000;
+
+        var watt = current * volt;
+
+        $("#total_power_output").html(NRG.formatOutputNumber(watt) +' W');
+        $("#total_volt_data").html(NRG.formatOutputNumber(volt)+ ' V');
+        $("#total_current_data").html(NRG.formatOutputNumber(current)+ ' A');
+
+        //console.log(data);
+
+    });
+
+    NRG.socket.on('updates', function (data) {
+        //console.log(data);
+
+        var updateRows = data.data;
+
+        updateRows.sort(function(a,b) {
+
+            // assuming distance is always a valid integer
+            return a.device - b.device;
+
+        });
+
+        //console.log(updateRows);
+        var inputPower = 0;
+        var outputPower = 0;
+        var rows = '';
+        $.each(updateRows, function(index, element) {
+            var volt = new Decimal(element['volt']);
+            var power = new Decimal(element['power']);
+            var watt = new Decimal(element['watt']);
+
+            rows += '<tr><td>'+element['device']+'</td><td>'+volt+' V</td><td>'+power+' A</td><td>'+watt+' W</td></tr>';
+
+            if(watt > 0) {
+                inputPower = +inputPower + +element['watt'];
+            } else {
+                outputPower = +outputPower + +element['watt'];
+            }
+
+        });
+
+        var outputPowerDisplay = Math.round(outputPower * 100000) / 100000;
+        var inputPowerDisplay = Math.round(inputPower * 100000) / 100000;
+        $("#power_output").html(outputPowerDisplay + ' W');
+        $("#total_incoming_power").html(inputPowerDisplay + ' W');
+
+        $("#deviceTable").html(rows);
+
+    });
+
+
+});
